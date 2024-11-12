b46d6502e1bce1a7898549595eb25151206b782c
Nigusu Yenework <nyenework@microsoft.com>
2024-11-12 15:20:20 -0800

    fix

--- test/NuGet.Core.Tests/NuGet.Protocol.Tests/Providers/PackageDetailsUriResourceV3ProviderTests.cs
+++ test/NuGet.Core.Tests/NuGet.Protocol.Tests/Providers/PackageDetailsUriResourceV3ProviderTests.cs
@@ -121,8 +121,9 @@ namespace NuGet.Protocol.Providers.Tests
                     new JProperty("@vocab", "http://schema.nuget.org/schema#"),
                     new JProperty("comment", "http://www.w3.org/2000/01/rdf-schema#comment")));
 
-            var serviceIndexResource = new ServiceIndexResourceV3(index, DateTime.UtcNow);
-            serviceIndexResource._allowInsecureConnections = true;
+            PackageSource source = new PackageSource("https://unit.test");
+            source.AllowInsecureConnections = true;
+            var serviceIndexResource = new ServiceIndexResourceV3(index, DateTime.UtcNow, source);
             var tryCreateResult = new Tuple<bool, INuGetResource>(true, serviceIndexResource);
 
             provider.Setup(x => x.TryCreate(It.IsAny<SourceRepository>(), It.IsAny<CancellationToken>()))
