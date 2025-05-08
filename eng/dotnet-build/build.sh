09671511b57469619f3e4fd305e75a2b2000be88
Viktor Hofer <viktor.hofer@microsoft.com>
2025-05-08 10:31:46 +0200

    fix arg

--- eng/dotnet-build/build.sh
+++ eng/dotnet-build/build.sh
@@ -51,7 +51,7 @@ done
 
 function ReadGlobalVersion {
   local key=$1
-  local global_json_file="$repo_rootglobal.json"
+  local global_json_file="$repo_root/global.json"
 
   if command -v jq &> /dev/null; then
     _ReadGlobalVersion="$(jq -r ".[] | select(has(\"$key\")) | .\"$key\"" "$global_json_file")"
